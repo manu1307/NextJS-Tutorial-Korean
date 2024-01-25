6ca1ac8f537284db91a5c381a9511618d8911c59
Sung Chul Park <jasonsc@naver.com>
2024-01-25 17:04:17 +0900

    Feat : chapter 5 ArticleTop 추가

--- app/tutorials/navigating-between-pages/page.tsx
+++ app/tutorials/navigating-between-pages/page.tsx
@@ -1,3 +1,4 @@
+import ArticleTop from "@/app/components/ui/Tutorial/article-top";
 import NavBar from "@/app/components/ui/Tutorial/nav-bar";
 import { contents } from "@/app/lib/contents";
 import React from "react";
@@ -8,6 +9,9 @@ function Page() {
   return (
     <>
       <NavBar chapter={data.chapter} title={data.title} />
+      <article className="mt-8 w-full min-w-0 max-w-6xl px-1 md:px-6 text-left">
+        <ArticleTop data={data} />
+      </article>
     </>
   );
 }
