7d12bd64bdc3a256ccc4333f149d9f3145fa51ee
Sung Chul Park <jasonsc@naver.com>
2024-01-25 17:03:27 +0900

    Feat : chapter 5 NavBar 추가

--- app/tutorials/navigating-between-pages/page.tsx
+++ app/tutorials/navigating-between-pages/page.tsx
@@ -1,7 +1,15 @@
+import NavBar from "@/app/components/ui/Tutorial/nav-bar";
+import { contents } from "@/app/lib/contents";
 import React from "react";
 
 function Page() {
-  return <div>Page</div>;
+  const data = contents[4];
+  const nextData = contents[5];
+  return (
+    <>
+      <NavBar chapter={data.chapter} title={data.title} />
+    </>
+  );
 }
 
 export default Page;
